5ec2684fca42278d0975485c997fc448207f18f2
riptideiv <riptide.xrc@gmail.com>
2025-12-23 16:50:38 -0800

    prepare chassis.hpp for adding improved odometry

--- src/chassis.cpp
+++ src/chassis.cpp
@@ -1,11 +1,15 @@
 #include "chassis.hpp"
 #include "main.h"
 #include "pros/motors.h"
+#include "pros/rotation.hpp"
 
 namespace chass {
     pros::MotorGroup mright({1, 3, -2}), mleft({-4, -9, 5});
+    pros::Rotation horizEnc(11), vertEnc(14);
     const double gearRatio = 3.0 / 4.0; // output revs / motor revs
     const double tuningK = 3;
+
+    const double horizOffset = 1.0, vertOffset = 2.4; // horiz offset = rightward is +, vert offset = forward is +
 
     double getLeftPos() { // left motors position in inches
         double ans = 0;
